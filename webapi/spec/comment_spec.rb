eea083d33d7a859891d060a3a49031d980fafb77
t-iwano <t-iwano@axsh.net>
2015-05-13 16:39:38 +0900

    Fixed Unit test.

--- webapi/spec/comment_spec.rb
+++ webapi/spec/comment_spec.rb
@@ -6,10 +6,11 @@ describe Webapi::Models::Comment do
   it "create a new comment." do
     comment = Webapi::Models::Comment.create(:display_name => "spec test",
                                              :comment => "test comment.")
+    expect(comment).to eq(Webapi::Models::Comment[comment.id])
   end
 
-  # it "create a new comment 2." do
-  #   comment = Webapi::Models::Comment.create(:display_name => "spec test 2",
-  #                                            :comment => "test comment.")
+  # it "validation display_name size must be 10 or less." do
+  #   expect { Webapi::Models::Comment.create(:display_name => "spec test 2",
+  #                                           :comment => "test comment.") }.to raise_error(Sequel::ValidationFailed)
   # end
 end
