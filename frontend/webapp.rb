94e12db29de4ee1752fca18ce6d9e3835c037e31
t-iwano <t-iwano@axsh.net>
2015-05-14 19:35:03 +0900

    Added validation check for frontend.

--- frontend/webapp.rb
+++ frontend/webapp.rb
@@ -3,19 +3,31 @@
 require 'sinatra/base'
 
 class WebApp < Sinatra::Base
+
+  configure do
+    use Rack::Session::Pool
+  end
+
   helpers do
     include Rack::Utils
     alias_method :h, :escape_html
   end
 
   get '/' do
+    @errors = session[:errors]
+    session.clear
+
     @comments = Comment.dataset.all
     erb :index
   end
 
   post '/comment' do
+    begin
     Comment.create(:display_name => params["display_name"],
                    :comment => params["comment"])
+    rescue Sequel::ValidationFailed => e
+      session[:errors] = e
+    end
     redirect '/'
   end
 end
