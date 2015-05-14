98e57ea50589a77a290cb370e9e041cc76d68bc4
t-iwano <t-iwano@axsh.net>
2015-05-15 08:56:28 +0900

    Fixed typo.

--- webapi/lib/webapi/endpoints/0.0.1/comments.rb
+++ webapi/lib/webapi/endpoints/0.0.1/comments.rb
@@ -4,7 +4,7 @@ require 'webapi/endpoints/0.0.1/responses/comments'
 
 Webapi::Endpoints::V001::Webapi.namespace '/comments' do
   get do
-    # description 'Show list fot the comments.'
+    # description 'Show list for the comments.'
     comments = M::Comment.dataset
     collection_respond_with(comments) do |c|
       R::CommentCollection.new(c).generate
